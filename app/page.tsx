3e478d636c5c8b6a8bc819c652f41e9371d9faf6
Muhammed Mishab <md.mishab124@gmail.com>
2026-08-30 12:45:42 +0530

    update about section

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/Hero";
 import Standard from "@/components/Standard";
 import Work from "@/components/Work";
 import WhyUs from "@/components/WhyUs";
+import About from "@/components/About";
 import Offer from "@/components/Offer";
 import FAQ from "@/components/FAQ";
 import IntakeForm from "@/components/IntakeForm";
@@ -18,6 +19,7 @@ export default function Home() {
         <Standard />
         <Work />
         <WhyUs />
+        <About />
         <Offer />
         <FAQ />
         <IntakeForm />
